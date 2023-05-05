5166080e99791d58c95da423a276a3325f7d3cce
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-05-05 08:45:01 +0000

    removed comment

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,7 +1,6 @@
 import { UserRead } from "@/api/openapi";
 import "react-auth-kit";
 
-// Register your router for maximum type safety
 declare module "react-auth-kit" {
   export function useAuthUser(): () => UserRead | null;
 }
