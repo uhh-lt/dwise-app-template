507bc3fa985e232227317add3391536f07f80d04
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-05-05 07:53:20 +0000

    added flex layout css to layouts

--- src/layouts/TopBottomNavigation/TopBottomNavigationLayout.tsx
+++ src/layouts/TopBottomNavigation/TopBottomNavigationLayout.tsx
@@ -1,12 +1,15 @@
 import { BottomNavigationBar, TopNavigationBar } from "@/components/Navigation";
+import { Box } from "@mui/material";
 import { Outlet } from "@tanstack/router";
 
 export function TopBottomNavigationLayout() {
   return (
-    <>
-      <TopNavigationBar />
-      <Outlet />
-      <BottomNavigationBar />
-    </>
+    <Box className="flexContainer">
+      <TopNavigationBar className="fixedContent" />
+      <Box className="flexibleContent">
+        <Outlet />
+      </Box>
+      <BottomNavigationBar className="fixedContent" />
+    </Box>
   );
 }
--- src/layouts/TopNavigation/TopNavigationLayout.tsx
+++ src/layouts/TopNavigation/TopNavigationLayout.tsx
@@ -1,11 +1,14 @@
 import { TopNavigationBar } from "@/components/Navigation";
+import { Box } from "@mui/material";
 import { Outlet } from "@tanstack/router";
 
 export function TopNavigationLayout() {
   return (
-    <>
-      <TopNavigationBar />
-      <Outlet />
-    </>
+    <Box className="flexContainer">
+      <TopNavigationBar className="fixedContent" />
+      <Box className="flexibleContent">
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
